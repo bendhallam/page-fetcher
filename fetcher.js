3612c8362df5119cd6762a5648fd71a19120b22a
Benjamin <bendhallam@gmail.com>
2024-07-13 17:05:12 -0600

    added comments

--- fetcher.js
+++ fetcher.js
@@ -2,17 +2,21 @@ const needle = require('needle');
 const fs = require('fs');
 
 const fetcher = (url, localPath) => {
+  // grab data from specified url
   needle.get(url, (error, response) => {
+    // check for errors and display them if present
     if (error) {
       console.error('Error fetching URL:', error);
       return;
     }
-    // Ensure you are writing the 'body' of the response
+    // write the body of the url to specified path
     fs.writeFile(localPath, response.body, (err) => {
+      // check for errors and display them if present
       if (err) {
         console.error('Error writing to file:', err);
         return;
       }
+      // log the size of the file and the new local path
       console.log(`Downloaded and saved ${response.body.length} bytes to ${localPath}`);
       return;
     });
